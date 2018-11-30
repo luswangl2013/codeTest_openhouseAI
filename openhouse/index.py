d459e18348bc1eab897fe2e5feeaabac3ec3beff
luswangl2013 <34317798+luswangl2013@users.noreply.github.com>
2018-11-30 16:45:54 -0700

    Update index.py

--- openhouse/index.py
+++ openhouse/index.py
@@ -44,15 +44,23 @@ def result():
                 occurrence["line"] = i+1
                 occurrence["start"] = index + 1
                 occurrence["end"] = index + len(string) + 1
-                if i == 0:#extend the line to define sentence
+                if i == 10:#extend the line to define sentence
                     origin = text[i] + " " + text[i+1]
+                elif i < 10:
+                    origin = text[i]
                 elif i == len(text) - 1:
                     origin = text[i-1] + " " + text[i]
                 else:
                     origin = text[i-1] + " " + text[i]+ " " + text[i+1]
                 
-                if re.search(regex, origin) == None:#no sentence segment in extended text
+                if i < 10:#no sentence segment in extended text
                     occurrence["in_sentence"] = text[i]
+                elif i == 10:
+                    pos = index
+                    end = pos + len(string) + 1
+                    while origin[end] not in ".?!":
+                        end += 1
+                    occurrence["in_sentence"] = origin[:end+1]
                 else:
                     '''
                     sentence_end = re.search(regex, origin).span()
